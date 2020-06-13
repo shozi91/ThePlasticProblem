12a6b4e4dfa13666a8f6618f6ef99d8e4c0a71f7
gustavhempel <t.mahn@oyacapitalinc.com>
2020-06-13 12:34:49 -0400

    js var

---
+++ templates/js/home.js
@@ -0,0 +1,8 @@
+var global_plastics_production= table1
+var impactstudies= table2
+var plastic_fate  = table3
+var plastic_waste_by_sector = table4
+var plastic_waste_generation_total = table5
+var summary_earth = table6
+var surface_plastic_mass_by_ocean = table7
+var mwl = table8
