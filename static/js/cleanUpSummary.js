7aa09070c0f3673daf96c36e33a01b12c3d4f304
shozi91 <shozi91@gmail.com>
2020-06-17 06:00:24 -0400

    Added title and cleaned up JS to make the graph look easier on the eyes'

--- static/js/cleanUpSummary.js
+++ static/js/cleanUpSummary.js
@@ -1,34 +1,51 @@
-
+tableData = []
+colorScale = []
  d3.json("/summary_earth").then(function(data){
-    tableData = []
-    data.forEach(function(data){
+     var dataLength = data.length
+    data.forEach(function(data,index){
          catItems = data.Categorized_Items
          totals = data.Total_Items
         var d = {
             label: catItems,
             value: totals
         }
-
+        var color = 0
+        if(dataLength < 100){
+            color = index/100
+        }
+        else{
+            color = index/dataLength
+        }
+        
          tableData.push(d)
+         colorScale.push(d3.interpolateViridis(color))
      })
      tableData.sort(function(first,second){
          return second.value-first.value
      });
+
+
     const options = {
         block: {
             dynamicHeight: true,
             highlight: true,
             minHeight: 40,
+            fill:{
+                scale: colorScale
+            }
         },
         chart: {
-            width: 1000,
+            width : Math.round((window.innerWidth)*0.75),
             height: 1900,
             animate:1,
             curve:{
                 enabled: true,
-                height: 10
+                height: 30
             }
 
+        },
+        label:{
+            fontFamily: "Open Sans",
         }
     };
     
@@ -36,7 +53,3 @@
     chart.draw(tableData, options);
     console.log(tableData)
  });
-
-
-
-
