101807b3db5b839bf37b69114b81534ce46421de
Dan Chang <daniel.hiensin.chang@gmail.com>
2020-06-19 23:40:13 -0400

    updated style for charts

--- static/js/source_final.js
+++ static/js/source_final.js
@@ -2,95 +2,106 @@
 // FOR PIE CHART
 
 
-d3.json("/plastic_waste_by_sector").then(function(data){
-    console.log(data)
+d3.json("/plastic_waste_by_sector").then(function (data) {
+  console.log(data)
+
+  var labels = []
+  var values = []
+
+  data.forEach(function (data) {
+    labels.push(data.Entity)
+    values.push(data.Primary_Plastic_Waste_Generation_tonnes)
+  });
+
+
+
+  var trace1 = {
+    labels: labels,
+    values: values,
+    marker: {
+      colors: ["#fd948d","#fe7067","#fd5042","#fe4132","#ed2617","#e22113","#cb1b0e","#bf170a","#b61509","#a51207","#991107","#840d04","#7a0b03","#670901","#5c0702","#470500","#3b0300","#290200"]
+    },
     
-    var labels = []
-    var values = []
+    type: 'pie'
+  };
 
-    data.forEach(function(data){
-        labels.push(data.Entity)
-        values.push(data.Primary_Plastic_Waste_Generation_tonnes)
-    });
+  var data = [trace1];
 
-    
+  var layout = {
+    plot_bgcolor:"#eee",
+    paper_bgcolor:"#eee"
+  };
 
-    var trace1 = {
-        labels: labels,
-        values: values,
-        type: 'pie'
-      };
-      
-      var data = [trace1];
-      
-      var layout = {
-      };
-      
-      Plotly.newPlot("plot", data, layout);
+  Plotly.newPlot("plot", data, layout);
 
-      console.log(labels)
-      console.log(values)
+  console.log(labels)
+  console.log(values)
 
- });
+});
 
 // FOR STACKED BAR CHART ***IF WE HAVE TIME, LOAD NEW FILE TO GET VOLUME AS OPPOSED TO %
 
 
- d3.csv("static/data/global_plastics_production_and_waste.csv").then(function(data){
-    console.log(data)
-    
-    var x = []
-    var y1 = []
-    var y2 = []
-    var y3 = []
+d3.csv("static/data/global_plastics_production_and_waste.csv").then(function (data) {
+  console.log(data)
 
-    data.forEach(function(data){
-        x.push(data.Year)
-        y1.push(data.Discarded_tonnes)
-        y2.push(data.Incinerated_tonnes)
-        y3.push(data.Recycled_tonnes)
-    });
+  var x = []
+  var y1 = []
+  var y2 = []
+  var y3 = []
 
-        var trace1 = {
-            x: x,
-            y: y1,
-            name: 'Discarded (Tonnes)',
-            type: 'bar'
-        };
-      
-        var trace2 = {
-            x: x,
-            y: y2,
-            name: 'Incinerated (Tonnes)',
-            type: 'bar'
-        };
+  data.forEach(function (data) {
+    x.push(data.Year)
+    y1.push(data.Discarded_tonnes)
+    y2.push(data.Incinerated_tonnes)
+    y3.push(data.Recycled_tonnes)
+  });
 
-        var trace3 = {
-            x: x,
-            y: y3,
-            name: 'Recycled (Tonnes)',
-            type: 'bar'
-        };
-      var data = [trace1, trace2, trace3];
-      
-      var layout = {barmode: 'stack'};
-      
-      Plotly.newPlot('stackbar', data, layout)
-    
- });
- 
+  var trace1 = {
+    x: x,
+    y: y1,
+    name: 'Discarded (Tonnes)',
+    type: 'bar'
+  };
 
- // FOR MAP
+  var trace2 = {
+    x: x,
+    y: y2,
+    name: 'Incinerated (Tonnes)',
+    type: 'bar'
+  };
+
+  var trace3 = {
+    x: x,
+    y: y3,
+    name: 'Recycled (Tonnes)',
+    type: 'bar'
+  };
+  var data = [trace1, trace2, trace3];
+
+  var layout = {
+    barmode: 'stack',
+    plot_bgcolor:"#eee",
+    paper_bgcolor:"#eee",
+    fillcolor: "#eee"
+  };
+
+  Plotly.newPlot('stackbar', data, layout)
+
+});
+
+
+// FOR MAP
 
 var geojson;
 
-function drawMap(data){
+function drawMap(data) {
 
   var myMap = L.map("map", {
     center: [29.081209, 1.123285],
-    zoom: 3
+    zoom: 2
   });
-  
+
   // Adding tile layer
   L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
@@ -108,7 +119,7 @@ function drawMap(data){
     valueProperty: "plastic_waste",
 
     // Set color scale
-    scale: ["#ffffff","#e8be29","#dc8416","#b10026"],
+    scale: ["#ffffff", "#e8be29", "#dc8416", "#b10026"],
 
     // Number of breaks in step range
     steps: 20,
@@ -123,7 +134,7 @@ function drawMap(data){
     },
 
     // Binding a pop-up to each layer
-    onEachFeature: function(feature, layer) {
+    onEachFeature: function (feature, layer) {
       layer.bindPopup("Country Name: " + feature.properties.name + "<br>Plastic Waste (tonnes):<br>" + feature.properties.plastic_waste);
     }
   }).addTo(myMap);
@@ -132,7 +143,7 @@ function drawMap(data){
 
   // Set up the legend
   var legend = L.control({ position: "bottomright", maxWidth: "10%" });
-  legend.onAdd = function() {
+  legend.onAdd = function () {
     var div = L.DomUtil.create("div", "info legend");
     var limits = geojson.options.limits;
     var colors = geojson.options.colors;
@@ -142,13 +153,13 @@ function drawMap(data){
     // Add min & max
     var legendInfo = "<h1>Plastic Waste (tonnes)</h1>" +
       "<div class=\"labels\">" +
-        "<div class=\"min\">" + limits[0] + "</div>" +
-        "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
+      "<div class=\"min\">" + limits[0] + "</div>" +
+      "<div class=\"max\">" + limits[limits.length - 1] + "</div>" +
       "</div>";
 
     div.innerHTML = legendInfo;
 
-    limits.forEach(function(limit, index) {
+    limits.forEach(function (limit, index) {
       labels.push("<li style=\"background-color: " + colors[index] + "\"></li>");
     });
 
@@ -161,7 +172,7 @@ function drawMap(data){
 }
 
 // Grab data with d3
-d3.json("/plastic_waste_generation_total").then(function(data) {
+d3.json("/plastic_waste_generation_total").then(function (data) {
   drawMap(data)
 
   console.log(data)
