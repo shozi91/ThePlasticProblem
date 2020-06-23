d5ed3074edbfa86f2bc15e3b528832e9b75776c3
Shehroze Ahmed <shozi91@gmail.com>
2020-06-23 14:24:37 -0400

    Update config.js
    added restricted API KEY

--- static/js/config.js
+++ static/js/config.js
@@ -1,1 +1,1 @@
-var API_KEY = "pk.eyJ1Ijoic2hvemk5MSIsImEiOiJja2IwMjh5cjMwMjRnMndueHVwazZwa2xyIn0.jZzK1v8yWim90KQLWwJCZw";
+var API_KEY = "pk.eyJ1Ijoic2hvemk5MSIsImEiOiJja2JzOTlwZ2EwMGdjMnVwcXllZ2t6YjAzIn0.YbZfhqoQ8_0PtD5K1JqT3w";
